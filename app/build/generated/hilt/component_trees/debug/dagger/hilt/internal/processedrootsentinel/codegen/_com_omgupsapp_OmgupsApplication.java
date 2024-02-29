706430abd450b00b891d418d4fd8ff67c68bbd0d
svodovv <90703451+svodovv@users.noreply.github.com>
2024-03-01 00:40:12 +0600

    Delete app/build directory

--- app/build/generated/hilt/component_trees/debug/dagger/hilt/internal/processedrootsentinel/codegen/_com_omgupsapp_OmgupsApplication.java
+++
@@ -1,9 +0,0 @@
-package dagger.hilt.internal.processedrootsentinel.codegen;
-
-import dagger.hilt.internal.processedrootsentinel.ProcessedRootSentinel;
-
-@ProcessedRootSentinel(
-    roots = "com.omgupsapp.OmgupsApplication"
-)
-public final class _com_omgupsapp_OmgupsApplication {
-}
